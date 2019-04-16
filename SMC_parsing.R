55b9ecf3adc3f1235fa947971ae1436eca5907fa
Lotus Lofgren <LLofgren@umn.edu>
2019-04-16 14:08:55 -0500

    Update SMC_parsing.R

--- SMC_parsing.R
+++ SMC_parsing.R
@@ -5,6 +5,7 @@ setwd("~/Desktop/Project_Suillus_comp_genomics/R")
 #load packages 
 library(tidyr)
 library(reshape2)
+library(dplyr)
 
 #read in the input files
 SMCs_DF<- read.csv("SMCs_Suillus_vs_OtherECM.csv")
@@ -13,8 +14,7 @@ SMCs_DF<- read.csv("SMCs_Suillus_vs_OtherECM.csv")
 #shrink DF
 SMC_DF_small<- SMCs_DF[,5:21]
 
-View(SMC_DF_small)
-#SMC_DF_small_no_zeros<- SMC_DF_small[, c(1:6, 9, 17)]
+SMC_DF_small_no_zeros<- SMC_DF_small[, c(1:6, 9, 17)]
 
 #shrink DF to the important stuff
 SMCs_DF_Suillus<- SMCs_DF[SMCs_DF$group == "S",]
@@ -56,6 +56,7 @@ spineplot(counts_gather_table, main = " ",
           #ylab = "", #xlab = "", 
           yaxlabels = NA, 
           xaxlabels = groups)
+
 
 #legend for spineplot
 legend("topleft", legend = colnames(counts_gather_table) , 
@@ -120,61 +121,6 @@ p + geom_point(position=position_jitterdodge(jitter.width = .13),alpha=0.3) +
 #####stats start here 
 #####ANOVA 
 
-#inspect data. 
-#test for variance heterogenety 
-var.test((as.numeric(SMC_DF_small_no_zeros$cf_fatty_acid) [SMC_DF_small_no_zeros$group == "S"]), (as.numeric(SMC_DF_small_no_zeros$cf_fatty_acid) [SMC_DF_small_no_zeros$group == "O"]))
-#not equal variance 
-
-View(SMC_DF_small_no_zeros)
-
-#build model
-genome_size_model <- lm(all_df$genome_size ~ all_df$group)
-
-#test normality and var
-par(mfrow = c(2,2)) #prints up to 4 plots on a 2 x 2 page
-plot(genome_size_model) #data is non normal with non constant variance
-par(mfrow = c(1,1)) #back to one image per page
-boxCox(genome_size_model, lambda = seq(-4, 1, 1/10)) #what does boxcox suggest? log transformation works. 
-
-#log transform to improve normality?
-all_df$genome_size_log<- log(all_df$genome_size)
-
-#see if the ransformation helped
-genome_size_model2 <- lm(all_df$genome_size_log ~ all_df$group)
-par(mfrow = c(2,2)) #prints up to 4 plots on a 2 x 2 page
-plot(genome_size_model2) #data is non normal with non constant variance
-par(mfrow = c(1,1)) #back to one image per page
-#not a lot, try -2 ==(1/x^2) - inverse square transformation 
-
-all_df$genome_size_inv_suqare<- all_df$genome_size^(-2)
-
-genome_size_model3 <- lm(all_df$genome_size_inv_suqare ~ all_df$group)
-par(mfrow = c(2,2)) #prints up to 4 plots on a 2 x 2 page
-plot(genome_size_model3) #data is non normal with non constant variance
-par(mfrow = c(1,1)) #back to one image per page
-#that looks a little better 
-
-#Run anova
-summary(aov(genome_size_model3))
-
-
-
-
-
-
-
-#set factors
-options(contrasts=c("contr.sum","contr.poly"))
-SMC_DF_small_no_zeros_long$SMC <- factor(SMC_DF_small_no_zeros_long$SMC,levels=c("cf_putative", "nrps", "other", "t1pks", "terpene", "indole", "cf_fatty_acid"))
-names(SMC_DF_small_no_zeros_long) <- c("Group", "SMC", "Count")
-SMC_DF_small_no_zeros_long$Group <- factor(SMC_DF_small_no_zeros_long$Group, levels=c("S", "O"))
-
-
-aov_results<- aov(SMC_DF_small_no_zeros_long$Count ~ SMC_DF_small_no_zeros_long$SMC * SMC_DF_small_no_zeros_long$Group, data = SMC_DF_small_no_zeros_long)
-summary(aov_results)
-
-TukeyHSD(aov_results)
-
 #type 2 ANOVA
 library("car")
 #make model
@@ -196,16 +142,141 @@ model.for.t4<- lm(SMC_DF_small_no_zeros_long$Count ~ SMC_DF_small_no_zeros_long$
 #take a look - lines in common mean not significantly different 
 sidelines(pairwise(model.for.t4, SMC_DF_small_no_zeros_long$Group_SMC_interaction,confidence = 0.95, type = "hsd"))
 
-#full result with p-vals:
+#full result with hsd -- note you can't get p-vals with hsd though with cfcdae. 
 pairwise(model.for.t4, SMC_DF_small_no_zeros_long$Group_SMC_interaction,confidence = 0.95, type = "hsd")
 
-#full result with p-vals:
+#run model for multiple t tests so that you can get p-vals
 pairs_out<- pairwise(model.for.t4, SMC_DF_small_no_zeros_long$Group_SMC_interaction,confidence = 0.95, type = "regwr")
 
 sidelines(pairs_out)
 #terpenes and Other are dignificantly different 
 
-#get p-value
+#get p-value using mulitple t tests and holm adjustment for multiple comparisons. 
 p_vals<-with(SMC_DF_small_no_zeros_long, pairwise.t.test(SMC_DF_small_no_zeros_long$Count,SMC_DF_small_no_zeros_long$Group_SMC_interaction,
                                      p.adjust.method="holm"))
 
+
+
+
+
+###### Within Suillus by host comparison ######
+
+#parse dataframe by host association
+#red
+red_SMC_df<- SMCs_DF_Suillus[SMCs_DF_Suillus$host == "R",]
+#white
+white_SMC_df<- SMCs_DF_Suillus[SMCs_DF_Suillus$host == "W",]
+#larch
+larch_SMC_df<- SMCs_DF_Suillus[SMCs_DF_Suillus$host == "L",]
+
+#format dataframs to relevent info
+#red
+red_SMC_df2<- red_SMC_df[,5:20]
+red_SMC_df3<- data.frame(round(colMeans(red_SMC_df2),0))
+names<- rownames(red_SMC_df3)
+red_SMC_df4<- cbind(red_SMC_df3, names, rep(("a_Red"), length(red_SMC_df3)))
+colnames(red_SMC_df4)<- c("counts", "names", "group")
+
+#white
+white_SMC_df2<- white_SMC_df[,5:20]
+white_SMC_df3<- data.frame(round(colMeans(white_SMC_df2),0))
+names<- rownames(white_SMC_df3)
+white_SMC_df4<- cbind(white_SMC_df3, names, rep(("b_White"), length(white_SMC_df3)))
+colnames(white_SMC_df4)<- c("counts", "names", "group")
+
+#larch
+larch_SMC_df2<- larch_SMC_df[,5:20]
+larch_SMC_df3<- data.frame(round(colMeans(larch_SMC_df2),0))
+names<- rownames(larch_SMC_df3)
+larch_SMC_df4<- cbind(larch_SMC_df3, names, rep(("c_Larch"), length(larch_SMC_df3)))
+colnames(larch_SMC_df4)<- c("counts", "names", "group")
+
+#bind them 
+SMCs_Suillus_DF<- rbind(red_SMC_df4, white_SMC_df4, larch_SMC_df4)
+
+#make repeating rows by SMC counts
+counts_gather2 <- as.data.frame(lapply(SMCs_Suillus_DF, rep, SMCs_Suillus_DF$counts))
+#wants to inherit zeros in the following- get rid of them by switching types (I dono)
+counts_gather3<- as.matrix(counts_gather2)
+counts_gather4<- as.data.frame(counts_gather3)
+
+#call table
+counts_gather_table<- table(counts_gather4$group, counts_gather4$names)
+
+groups<- c("Red", "White", "Larch")
+#spine plot - note - I removed hte light green color for "indole" as there are not indols in the Suillus group 
+par(las = 1)
+par(mar = c(6.5, 8.5, 3, 3.5), mgp = c(6, 2.5, 0))
+spineplot(counts_gather_table, main = " ", 
+          col=c("#5676A1", "#405952", "#FDD191", "#885053", "#B2675E", "#442B47"), 
+          border = NA, 
+          #cex.axis = 0.66, 
+          #ylab = "", #xlab = "", 
+          yaxlabels = NA, 
+          xaxlabels = groups)
+
+#legend for spineplot
+legend("topleft", legend = colnames(counts_gather_table) , 
+       fill=c("#5676A1", "#405952", "#FDD191", "#885053", "#B2675E", "#442B47"),
+       ncol = 1, 
+       cex = .4, 
+       lwd = 0, 
+       box.lwd = 0, 
+       box.lty =0, 
+       box.col =0, 
+       xjust = 1)
+
+
+#get n and N
+counts_gather_table
+N<- rowSums(counts_gather_table)
+N
+
+
+
+
+
+
+####make grouped box plot
+#first put into long form
+SMCs_DF_Suillus2<- SMCs_DF_Suillus[c(SMCs_DF_Suillus$host == "W" | SMCs_DF_Suillus$host == "R" | SMCs_DF_Suillus$host == "L"),]
+
+SMC_DF_small_no_zeros_Suillus<- SMCs_DF_Suillus2[, c(5:10, 22)]
+
+#add repeat col (ours is only 1)
+SMC_DF_small_no_zeros_Suillus$id<- seq(1:nrow(SMC_DF_small_no_zeros_Suillus))
+SMC_DF_small_no_zeros_Suillus <- SMC_DF_small_no_zeros_Suillus %>% group_by(host) %>%
+  gather(data = SMC_DF_small_no_zeros_Suillus, id, cf_fatty_acid, cf_putative, nrps, other, t1pks, terpene)
+
+#check class
+class(SMC_DF_small_no_zeros[1,1])
+
+#make col names match
+colnames(SMC_DF_small_no_zeros_Suillus)<- c("Group", "SMC", "Count")
+
+
+#set order of boxes
+SMC_DF_small_no_zeros_Suillus$Group <- factor(SMC_DF_small_no_zeros_Suillus$Group,
+                       levels = c('L','W', 'R'),ordered = TRUE)
+
+###make grouped box plot
+p<- ggplot(SMC_DF_small_no_zeros_Suillus, aes(x=SMC, y=Count, fill=Group)) + 
+  geom_boxplot()
+p+scale_fill_manual(values=c("#B1913A", "#273666", "#FCD090")) + theme_minimal()
+
+#horizontal
+p + geom_point(position=position_jitterdodge(jitter.width = .13),alpha=0.3) +
+  theme_bw(base_size = 3) + scale_fill_manual(values=c("#B1913A", "#273666", "#FCD090")) + theme_minimal() + coord_flip()
+
+
+####### Stats #######
+#type 2 ANOVA
+library("car")
+#make model
+model.for.t2_Suillus_only<-lm(SMC_DF_small_no_zeros_Suillus$Count ~ SMC_DF_small_no_zeros_Suillus$SMC * SMC_DF_small_no_zeros_Suillus$Group)
+
+model.for.t3<- Anova(model.for.t2_Suillus_only, type=2)
+
+summary(SMC_DF_small_no_zeros_long)
+
+#no significant difference between groups. 
